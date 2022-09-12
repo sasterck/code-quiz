a743344ce1351caea4a944ac905465564a76292a
[Sara Sterck] <[sasterck14@gmail.com]>
2022-09-11 18:29:51 -0600

    updates made to index, js, and css while on a flight so unable to push earlier

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 // Let's get this quiz on the road!
 
- {
-    
-}
+var quizMain = document.getElementById('quiz');
+
+var startButton = document.getElementById('start');
+
+var optionA = document.getElementById('A');
+
+var optionB = document.getElementById('B');
+
+var optionC = document.getElementById('C');
+
+var questionAnswer = document.getElementById('answer');
+
+var quizTimer = document.getElementById('timer');
+
+var scoreTotal = document.getElementById('Score');
+
+var quizEnd = document.getElementById('Fin');
+
+// Now, onto the more difficult part - adding the questions.
+
+var quizQuestions = [ {
+    Question: "How are you?"
+}]
